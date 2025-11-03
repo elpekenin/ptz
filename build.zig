fda5c48ab0c94a6a3bbcd975e2df311d88cd8fa5
elpekenin <elpekenin@elpekenin.dev>
2025-11-03 15:34:34 +0100

    add standard options

--- build.zig
+++ build.zig
@@ -5,7 +5,13 @@ const std = @import("std");
 // con: same exe can't query cards in different languages
 
 pub fn build(b: *std.Build) void {
+    // options
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     _ = b.addModule("ptz", .{
         .root_source_file = b.path("src/root.zig"),
+        .target =  target,
+        .optimize = optimize,
     });
 }
